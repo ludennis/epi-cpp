a62ff5c9c0437454117781592cd3463aae1bbcf6
Dennis Lu <Dennis.Lu@itri.org.tw>
2021-03-03 14:38:56 +0800

    TreeNode.h: added TreeNodeParent with parent pointer

--- ch10/TreeNode.h
+++ ch10/TreeNode.h
@@ -13,3 +13,31 @@ struct TreeNode
   std::unique_ptr<TreeNode<T>> left;
   std::unique_ptr<TreeNode<T>> right;
 };
+
+template <typename T>
+struct TreeNodeParent : public std::enable_shared_from_this<TreeNodeParent<T>>
+{
+  TreeNodeParent<T>(T t)
+  : data(t)
+  , parent(nullptr)
+  , left(nullptr)
+  , right(nullptr)
+  {}
+
+  void PushLeft(const T &t)
+  {
+    left = std::make_shared<TreeNodeParent<T>>(t);
+    left->parent = TreeNodeParent<T>::shared_from_this();
+  }
+
+  void PushRight(const T &t)
+  {
+    right = std::make_shared<TreeNodeParent<T>>(t);
+    right->parent = TreeNodeParent<T>::shared_from_this();
+  }
+
+  T data;
+  std::shared_ptr<TreeNodeParent<T>> parent;
+  std::shared_ptr<TreeNodeParent<T>> left;
+  std::shared_ptr<TreeNodeParent<T>> right;
+};
